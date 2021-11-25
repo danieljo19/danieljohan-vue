32dd34074c9ef8779e89a6a5a8d4ee23ed952943
danieljo19 <danieljohan37@gmail.com>
2021-11-25 09:31:33 +0700

    update build

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/js/bootstrap.bundle.min.js"
-import router from './router'
-
-const app = createApp(App);
-app.use(router);
-app.mount('#app');
