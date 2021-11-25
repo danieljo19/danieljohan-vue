50d852ea1157ca65903af3f277d577d1711982cd
danieljo19 <danieljohan37@gmail.com>
2021-11-25 19:10:03 +0700

    update

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.bundle.min.js"
 import router from './router'
 
-const app = createApp(App);
+const app = createApp(App)
 app.use(router);
 app.mount('#app');
